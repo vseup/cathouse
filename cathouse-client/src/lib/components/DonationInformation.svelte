66f3e36c46ecf39b8c9f74dba05ca41c08d76e46
vseup <hello@verenapues.com>
2026-02-15 12:52:13 +0100

    client: donation optiones: removed banking option

--- cathouse-client/src/lib/components/DonationInformation.svelte
+++ cathouse-client/src/lib/components/DonationInformation.svelte
@@ -2,25 +2,11 @@
 	import Button from '$lib/components/controls/Button.svelte';
 	import Icon from '$lib/components/controls/Icon.svelte';
 	import Spacer from '$lib/components/Spacer.svelte';
-	import QrBanking from '$lib/assets/images/qr_banking.jpg';
 	import QrPaypal from '$lib/assets/images/qr_paypal.jpg';
 	import Divider from '$lib/components/Divider.svelte';
 </script>
 
 <div class="col">
-	<b>Überweisung</b>
-	<Spacer height={8} />
-	<div class="row">
-		<div class="col iban">
-			<p>Tierschutzverein Starnberg u. U. e. V.</p>
-			<p>Kreissparkasse München Starnberg</p>
-			<p>IBAN: DE25702501500430056762</p>
-			<p>BIC: BYLADEM1KMS</p>
-		</div>
-		<div style="flex: 1"></div>
-		<img class="qr" src={QrBanking} alt="QR Code mit Bank Informationen des Tierheim Starnbergs" />
-	</div>
-	<Divider />
 	<b>Paypal</b>
 	<Spacer height={8} />
 	<div class="row">
@@ -60,9 +46,3 @@
 		<Icon icon="open_in_new" weight={300} />
 	</div>
 </div>
-
-<style>
-	.iban {
-		line-height: 1.4;
-	}
-</style>

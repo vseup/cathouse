e080a8a9b09b8d6cb1543c73b37bc42deba65e2f
vseup <hello@verenapues.com>
2026-02-15 17:39:43 +0100

    api: fix seed

--- cathouse-api/prisma/seed.ts
+++ cathouse-api/prisma/seed.ts
@@ -1,29 +1,32 @@
 import { CatType } from '../src/generated/prisma/client'
 import { prisma } from './prisma'
+
+const cats = [
+    {
+        name: "Heribert",
+        donor: "Max Mustermann",
+        type: CatType.CAT01,
+        donation: 100.0
+    },
+    {
+        name: "Stella",
+        type: CatType.CAT02,
+        donation: 900.0
+    },
+    {
+        name: "Nachtschatten",
+        type: CatType.CAT03,
+        donation: 50.0
+    },
+]
+
 async function main() {
-    await prisma.cat.create({
-        data: {
-            name: "Heribert",
-            donor: "Max Mustermann",
-            type: CatType.CAT01,
-            donation: 100.0
-        },
-    })
-    await prisma.cat.create({
-        data: {
-            name: "Stella",
-            type: CatType.CAT02,
-            donation: 900.0
-        },
-    })
-    await prisma.cat.create({
-        data: {
-            name: "Nachtschatten",
-            type: CatType.CAT03,
-            donation: 50.0
-        },
+    await prisma.cat.createMany({
+        data: cats,
+        skipDuplicates: true,
     })
 }
+
 main()
     .then(async () => {
         await prisma.$disconnect()
